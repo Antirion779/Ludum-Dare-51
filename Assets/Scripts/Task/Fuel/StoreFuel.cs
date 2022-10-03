d88fe110b629a3159285a8bee4378ac34097498f
Kaito_OwO <advestankv@gmail.com>
2022-10-03 11:00:24 +0200

    Yes

--- Assets/Scripts/Task/Fuel/StoreFuel.cs
+++ Assets/Scripts/Task/Fuel/StoreFuel.cs
@@ -9,40 +9,69 @@ public class StoreFuel : MonoBehaviour
 
     public string currentFuel = "None";
     public GameObject manager;
-    public Slider slider;
-    public Image fillColor;
-    public TextMeshProUGUI text;
+    public SpriteRenderer slider;
+    public string fuelAccepted = "None";
     public float fillSpeed = 0.01f;
+    public float totalFill = 0f;
+    public static StoreFuel Instance;
+
+    public GameObject coffee, water, gas;
+
+    private void Awake()
+    {
+        Instance = this;
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         currentFuel = collision.name;
-        switch (text.text)
+        switch (fuelAccepted)
         {
             case "Coffee":
-                fillColor.color = new Color(0.4901961f, 0.2784314f, 0);
+                slider.color = new Color(0.4901961f, 0.2784314f, 0);
                 break;
 
             case "Gas":
-                fillColor.color = new Color(0.764706f, 0.7450981f, 0);
+                slider.color = new Color(0.764706f, 0.7450981f, 0);
                 break;
 
             case "Water":
-                fillColor.color = new Color(0f, 0.7686275f, 0.9960785f);
+                slider.color = new Color(0f, 0.7686275f, 0.9960785f);
                 break;
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (currentFuel == text.text)
+        if (currentFuel == fuelAccepted)
         {
-            slider.value += fillSpeed;
-            if(slider.value >= 1)
+            totalFill += fillSpeed;
+            if(totalFill >= 1)
             {
+                GameManager.Instance.reset();
                 Destroy(manager, 1f);
             }
         }
+    }
+
+    public void ActivateFuel() {
+
+        switch (fuelAccepted)
+        {
+            case "Coffee":
+                coffee.SetActive(true);
+                break;
+
+            case "Gas":
+                gas.SetActive(true);
+                break;
+
+            case "Water":
+                water.SetActive(true);
+                break;
+        }
+
     }
 
 
